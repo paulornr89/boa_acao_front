cc196650ded7d86a76b312d822cbdfce1fd0706b
paulornr89 <paulornr89@gmail.com>
2025-10-13 22:10:08 -0300

    incluido componente para itens

---
+++ src/components/Itens/Itens.jsx
@@ -0,0 +1,10 @@
+export default function Itens() {
+    return <div className="mostrarItens">
+            <div className="titulo">
+                <h2 className="titulo-texto">Itens</h2>
+            </div>
+            <div className="listaItens">
+            </div>
+        </div>;
+}
+
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 
 import CadastroDeUsuario from '../components/Cadastro/Cadastro.jsx'
 import Login from '../components/Login/Login.jsx';
+import Itens from '../components/Itens/Itens.jsx';
 
 export default function AppRoutes() {
     return (<Routes>
       <Route path="/" element={ <h1>APP BOA AÇÃO</h1> } />
       <Route path="/login" element={ <Login/> } />
       <Route path="/cadastro" element={ <CadastroDeUsuario/> } />
+      <Route path="/itens" element={ <Itens/> } />
     </Routes>);
 }
