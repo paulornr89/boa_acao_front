98f5afca28ff039561874980203d4c819effcb63
paulornr89 <paulornr89@gmail.com>
2025-12-15 14:05:38 -0300

    carregando imagem dos itens no front

--- src/components/Item/CardItem.jsx
+++ src/components/Item/CardItem.jsx
@@ -1,4 +1,4 @@
-import itens from '../../assets/balde.png';
+import itensDefault from '../../assets/balde.png';
 import editar from '../../assets/editar.png';
 import excluir from '../../assets/excluir.png';
 import salvar from '../../assets/salvar.png';
@@ -7,6 +7,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { ItemContext } from '../../context/ItemContext.jsx';
 import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import Input from '../Fields/Input.jsx';
+
 
 export default function CardItem({item}) {
     const [ editarItem, setEditarItem ] = useState(false);
@@ -18,13 +19,20 @@ export default function CardItem({item}) {
     const nomeRef = useRef();
     const unidadeRef = useRef();
     const descricaoRef = useRef();
-
+    const temImagem = item.media && item.media.length > 0;
+    const imagemSrc = temImagem ? item.media[0].source : itensDefault;
+    console.log(item.media[0].source)
     useEffect(() => {}, [editarItem]);
 
     return  <div className='w-130 bg-primary flex gap-2 justify-around flex-col h-90 p-5 rounded-md shadow-[1px_1px_5px_rgba(0,0,0,0.3)] transition-transform 
             duration-300 hover:scale-103"'>
                 <div className=' flex justify-around'>
-                    <img className="w-25 self-center" src={itens}/>
+                    <img 
+                        className="w-32 h-32 object-cover rounded-md border-2" 
+                        src={imagemSrc}
+                        alt={item.nome}
+                        onError={(e) => { e.target.src = itensDefault; }} // Fallback de segurança se a imagem quebrar
+                    />
                     <div className='w-80 flex flex-col justify-center text-center text-xl'>
                         {
                             editarItem ?
