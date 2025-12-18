bc53529bf13318d1ef2c91c4af9de005de04eb75
paulornr89 <paulornr89@gmail.com>
2025-12-18 15:45:10 -0300

    ajuste para a responsividade pela tela da aplicação

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,12 @@ export default function Home() {
     return <>
         <header className="flex items-center justify-between bg-secundary h-20 pl-5 pr-5">                
             {/* <img className='h-15 w-15 rounded-md bg-white' src={logo}/> */}
-            <h2 className="text-3xl text-white font-bold text-shadow-lg/20">Boa Ação - O destino certo para sua doação!</h2>
+            <h1 className="font-bold text-white leading-tight
+                        text-xs           /* Celular: Texto bem pequeno */
+                        sm:text-sm        /* Tablet pequeno: Um pouco maior */
+                        md:text-xl        /* PC/Tablet: Texto grande */
+                        max-w-[150px]     /* Limita largura no celular para quebrar linha se precisar */
+                        md:max-w-none font-bold text-shadow-lg/20">Boa Ação - O destino certo para sua doação!</h1>
             <Link className="text-white font-bold text-lg hover:text-content text-shadow-lg" to={`/login`}>
                 Acessar
             </Link>
